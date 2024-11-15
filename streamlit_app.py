323755996c7550b6040b8a5b97203023704df85d
ellen-firs <148352828+ellen-firs@users.noreply.github.com>
2024-11-15 19:23:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
-
+st.html(
+    "<head>
+  <!-- ... -->
+  <script src="https://telegram.org/js/telegram-web-app.js"></script>
+  <!-- ... other scripts -->
+</head>"
+)
 st.write('Hello world!')
